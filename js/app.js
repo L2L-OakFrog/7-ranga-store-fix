6dcc1c0eb0f5797ea42e0e351c84af47a17e8977
L2L_OakFrog <rafiqahamed.tanim@gmail.com>
2021-09-13 20:12:38 +0600

    finishing up

--- js/app.js
+++ js/app.js
@@ -46,12 +46,18 @@ const loadProducts = () => {
 // products data (end)
 
 // Single details
-const detailsInfo = (id) => {
+/* const detailsInfo = (id) => {
   const url = `https://fakestoreapi.com/products/${id}`
   fetch(url)
             .then(res=>res.json())
             .then(json=>console.log(json)) // shows in console
+  showDetails();
 }
+
+const showDetails = () =>{
+  
+} */
+// Single details
 
 // Show all product in UI (start)
 const showProducts = (products) => {
@@ -71,7 +77,7 @@ const showProducts = (products) => {
       <p><strong>Total Rated:</strong> ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button onclick="detailsInfo(${id})" id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="detailsInfo(${id})" id="details-btn" class="btn btn-secondary">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
